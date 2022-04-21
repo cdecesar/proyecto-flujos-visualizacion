cb71eb2b3317d933907aa677ae4af72d9176fc9c
100429095@alumnos.uc3m.es <Pesados21@>
2022-04-21 09:34:56 +0200

    Todos los cambios acabados

--- Visualizacion_1Sector.py
+++ Visualizacion_1Sector.py
@@ -4,6 +4,7 @@ import re
 import json
 import pandas
 import pathlib
+import webbrowser
 from shapely.geometry import Polygon
 from funciones import obtener_poligono, obtener_datos_poligono, create_popup_info, auto_open
 
@@ -24,29 +25,28 @@ class Visualizacion():
 
         self.FILES_PATH = str(pathlib.Path(__file__).parent.resolve())
 
-        self.cargar_info()
-        self.cargar_un_mapa()
-        self.mostrar()
-
-    def cargar_info(self):
         if len(self.sector) == 1:
-            with open(self.FILES_PATH + '\\Carpeta_JSON\\Clusterizados2_' + self.sector[0] + '.json', "r") as f6:
-                self.json_flujos = json.load(f6)
-            f6.close()
-
-            with open(self.FILES_PATH + '\\JSON_Cargados\\Pamplona\\Asociacion2_' + self.sector[0] + '.json', "r") as f6:
-                self.json_aeronaves = json.load(f6)
-            f6.close()
-
-            path_poligono = self.FILES_PATH + '\\Archivos\\poligonos_sectores_2D_2.0.csv'
-            fichero_poligonos = pandas.read_csv(path_poligono, delimiter=';')
-            datos_poligonos = fichero_poligonos['PolygonToString']
-
-            coordenadas_poligono_raw = re.split(",", datos_poligonos[obtener_datos_poligono(fichero_poligonos, self.sector[0])])
-            self.lista_coordenadas = obtener_poligono(coordenadas_poligono_raw)
-            self.poligono1 = Polygon(self.lista_coordenadas)
+            self.cargar_info_un_mapa()
+            self.mostrar(self.cargar_un_mapa())
         else:
-            pass
+            self.mostrar(self.cargar_varios_mapas())
+
+    def cargar_info_un_mapa(self):
+        with open(self.FILES_PATH + '\\Carpeta_JSON\\Clusterizados2_' + self.sector[0] + '.json', "r") as f6:
+            self.json_flujos = json.load(f6)
+        f6.close()
+
+        with open(self.FILES_PATH + '\\JSON_Cargados\\Pamplona\\Asociacion2_' + self.sector[0] + '.json', "r") as f6:
+            self.json_aeronaves = json.load(f6)
+        f6.close()
+
+        path_poligono = self.FILES_PATH + '\\Archivos\\poligonos_sectores_2D_2.0.csv'
+        fichero_poligonos = pandas.read_csv(path_poligono, delimiter=';')
+        datos_poligonos = fichero_poligonos['PolygonToString']
+
+        coordenadas_poligono_raw = re.split(",", datos_poligonos[obtener_datos_poligono(fichero_poligonos, self.sector[0])])
+        self.lista_coordenadas = obtener_poligono(coordenadas_poligono_raw)
+        self.poligono1 = Polygon(self.lista_coordenadas)
 
     def cargar_un_mapa(self):
 
@@ -93,6 +93,81 @@ class Visualizacion():
             grupo_leyenda.add_to(self.mapa)
 
         folium.LayerControl().add_to(self.mapa)
+        return self.sector[0]
 
-    def mostrar(self):
-        auto_open(self.sector[0], self.mapa)
+    def mostrar(self, nombre):
+        path = 'Mapa-' + nombre + '.html'
+        html_page = f'{path}'
+        self.mapa.save(html_page)
+        new = 2
+        webbrowser.open(html_page, new=new)
+
+
+    def cargar_varios_mapas(self):
+
+        path_poligono = self.FILES_PATH + '\\Archivos\\poligonos_sectores_2D_2.0.csv'
+        fichero_poligonos = pandas.read_csv(path_poligono, delimiter=';')
+        datos_poligonos = fichero_poligonos['PolygonToString']
+
+        codigo_mapa = ''
+        for s in self.sector:
+            codigo_mapa += s + '-'
+        codigo_mapa = codigo_mapa[:-1]
+
+        codigos_sectores = {'LECMPAU': 'Pamplona', 'LECMCJI': 'Castejon', 'LECMCJL': 'Castejon',
+                                 'LECMCJU': 'Castejon', 'LECMTLI': 'Toledo', 'LECMTLU': 'Toledo',
+                                 'LECMDGU': 'Domingo', 'GCCCRNE': 'Canarias', 'LECMTZI': 'Zaragoza',
+                                 'LECMTZU': 'Zaragoza', 'LECBP2R': 'Barcelona_p2r'}
+
+        MAPA = 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmVsc2FyIiwiYSI6ImNsMWtnd3UyaTAwZGkzYm8zeng1ZHF6YXIifQ.zjyS9oluZL2c0sCP4krWFw'
+        colores = ['green', 'blue', 'red', 'yellow', 'black']
+
+        self.mapa = folium.Map(location=[42.41632777950267, -2.0189176134722495], tiles=None, zoom_start=8)
+        tile_layer = folium.TileLayer(
+            tiles=MAPA,
+            attr='mapbox',
+            zoom_start=8,
+            name=codigo_mapa,
+            control=True,
+            opacity=0.7
+        )
+        tile_layer.add_to(self.mapa)
+
+        for j in self.sector:
+
+            coordenadas_poligono_raw = re.split(",", datos_poligonos[obtener_datos_poligono(fichero_poligonos, j)])
+            lista_coordenadas = obtener_poligono(coordenadas_poligono_raw)
+
+            with open(self.FILES_PATH + '\\Carpeta_JSON\\Clusterizados2_' + j + '.json', "r") as f6:
+                json_flujos = json.load(f6)
+            f6.close()
+
+            with open(self.FILES_PATH + '\\JSON_Cargados\\' + codigos_sectores.get(j) + '\\Asociacion2_' + j + '.json',
+                      "r") as f6:
+                json_aeronaves = json.load(f6)
+            f6.close()
+
+            grupo_leyenda = folium.FeatureGroup('Sector: ' + str(j))
+
+            folium.Polygon(lista_coordenadas,
+                           color='white',
+                           weight=2,
+                           fill=True,
+                           # fill_color="white",
+                           fill_opacity=0.4).add_to(grupo_leyenda)
+
+            for i in json_flujos:
+                datos = json_flujos.get(i)
+                aeronaves = json_aeronaves.get(i)
+
+                iframe = folium.IFrame(create_popup_info(datos, i, aeronaves), height=300)
+                popup = folium.Popup(iframe, min_width=700, max_width=700)
+
+                folium.PolyLine([(datos[0][0][1], datos[0][0][0]), (datos[0][1][1], datos[0][1][0])],
+                                color=random.choice(colores), weight=random.randint(1, 4),
+                                tooltip='Flujo: ' + str(i), popup=popup).add_to(grupo_leyenda)
+
+            grupo_leyenda.add_to(self.mapa)
+
+        folium.LayerControl().add_to(self.mapa)
+        return codigo_mapa
